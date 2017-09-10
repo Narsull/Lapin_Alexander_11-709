b8e41318fc6f0c437258b0264b4e09c780ddaea4
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-10 20:25:21 +0300

    [add] Решено первое задание

---
+++ MISC (miscellaneous)/First tasks/Task_1/Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double A;
+            Double B;
+            Console.WriteLine("Напишите два числа");
+            Console.WriteLine();
+            Console.Write("A=");
+            A = Convert.ToDouble(Console.ReadLine());
+            Console.Write("B=");
+            B = Convert.ToDouble(Console.ReadLine());
+            A = A + B;
+            B = A - B;
+            A = A - B;
+            Console.WriteLine("A=" + A);
+            Console.WriteLine("B=" + B);
+            Console.Read();
+        }
+    }
+}
