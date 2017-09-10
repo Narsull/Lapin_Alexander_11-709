5c277d3480fa1be81d72eb9203c9a27cdfc60775
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-10 22:19:54 +0300

    [add] Добавлено незаконченное третье задание

---
+++ MISC (miscellaneous)/First tasks/Task_3/Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Chas;
+            Double Gradus;
+            Console.WriteLine("Введите час");
+            Chas = Convert.ToDouble(Console.ReadLine());
+            if (Chas > 12)
+            {
+                Chas = Chas - 12;
+            }
+            Gradus = Chas * 30;
+            if (Gradus > 360)
+                {
+                Gradus = Gradus - 360;
+            }
+            if (Chas % 12 == 0)
+            {
+                Gradus = 0;
+            }
+            Console.WriteLine("Угол между минутной и часовой стрелками равен " + Gradus);
+            Console.Read();
+        }
+    }
+}
