56b95aa05a028b61e5cf761b415f29f7fba12f97
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-11 12:41:14 +0300

    [add][fix] Решено пятое задание и изменены названия уже сделаных заданий

---
+++ FALL2017/MISC (miscellaneous)/First tasks/Task_5/Task_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr5
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a;
+            Double b;
+            Double c;
+            Console.WriteLine("Напишите временной отрезок от одного года до другого");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = b - a;
+            c = c / 4 ;
+            c = c - (c % 4);
+            Console.WriteLine("Количество високосных лет равно " + c);
+            Console.Read();
+        }
+    }
+}
