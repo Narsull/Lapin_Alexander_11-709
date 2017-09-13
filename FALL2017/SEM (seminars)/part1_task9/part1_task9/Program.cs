915680b0dbb851a029389c62d218011515320cf5
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-13 23:47:49 +0300

    [add] Начато решение 9 задания

---
+++ FALL2017/SEM (seminars)/part1_task9/part1_task9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1_task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+        }
+    }
+}
