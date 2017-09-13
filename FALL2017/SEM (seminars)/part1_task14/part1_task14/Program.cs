3b0c64a16138d8c971fefe00f1d77717d7c29d4c
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-13 20:57:04 +0300

    [fix][add] Изменена директория первых заданий и решено одно задание из семистровок

---
+++ FALL2017/SEM (seminars)/part1_task14/part1_task14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1_task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите номер билета");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            int a = (ticket / 100000) - ((ticket % 100000) / 100000); // Первая цифра
+            int b = (ticket / 10000) - (a * 10) - ((ticket % 10000) / 10000); // Вторая цифра
+            int c = (ticket / 1000) - (a * 100 + b * 10) - ((ticket % 1000) / 1000); // Третья цифра
+            int d = ((ticket % 1000) - (ticket % 100)) / 100; // Четвёртая цифра
+            int e = ((ticket % 100) - (ticket % 10)) / 10; // Пятая цифра
+            int f = ticket % 10; // Шестая цифра
+            int amount1 = a + b + c; // Сумма первых трёх цифр
+            int amount2 = d + e + f; // Сумма последних трёх цифр
+            if (amount1 == amount2)
+            {
+                Console.WriteLine("Поздравляем!");
+                Console.WriteLine("Ваш билет счастливый!");
+            }
+            else 
+            Console.WriteLine("К сожалению, ваш билет не является счастливым.");
+            Console.Read();
+        }
+    }
+}
