6ed9a706a0378446933cc09f95b32d2f1862e856
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-14 08:32:04 +0300

    [fix] Начата модернизация 14 задачи

--- FALL2017/SEM (seminars)/part1_task14/part1_task14/Program.cs
+++ FALL2017/SEM (seminars)/part1_task14/part1_task14/Program.cs
@@ -10,23 +10,28 @@ namespace part1_task14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напишите номер билета"); //123456
+            Console.WriteLine("Напишите номер билета");
             int ticket = Convert.ToInt32(Console.ReadLine());
-            int a = (ticket / 100000); // Первая цифра
-            int b = (ticket / 10000) - (a * 10); // Вторая цифра
-            int c = (ticket / 1000) - (a * 100 + b * 10); // Третья цифра
-            int d = ((ticket % 1000) - (ticket % 100)) / 100; // Четвёртая цифра
-            int e = ((ticket % 100) - (ticket % 10)) / 10; // Пятая цифра
-            int f = ticket % 10; // Шестая цифра
-            int amount1 = a + b + c; // Сумма первых трёх цифр
-            int amount2 = d + e + f; // Сумма последних трёх цифр
-            if (amount1 == amount2)
+            string str = (ticket.ToString());
+                if (str.Length )
             {
-                Console.WriteLine("Поздравляем!");
-                Console.WriteLine("Ваш билет счастливый!");
+                int a = (ticket / 100000); // Первая цифра
+                int b = (ticket / 10000) - (a * 10); // Вторая цифра
+                int c = (ticket / 1000) - (a * 100 + b * 10); // Третья цифра
+                int d = ((ticket % 1000) - (ticket % 100)) / 100; // Четвёртая цифра
+                int e = ((ticket % 100) - (ticket % 10)) / 10; // Пятая цифра
+                int f = ticket % 10; // Шестая цифра
+                int amount1 = a + b + c; // Сумма первых трёх цифр
+                int amount2 = d + e + f; // Сумма последних трёх цифр
+                if (amount1 == amount2)
+                {
+                    Console.WriteLine("Поздравляем!");
+                    Console.WriteLine("Ваш билет счастливый!");
+                }
+                else
+                    Console.WriteLine("К сожалению, ваш билет не является счастливым.");
             }
-            else 
-            Console.WriteLine("К сожалению, ваш билет не является счастливым.");
+            else Console.WriteLine("Введите шестизначный номер билета.");
             Console.Read();
         }
     }
