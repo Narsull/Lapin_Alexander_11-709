81182359169d3cf18cf7f4ab23991762e3519d4a
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-14 12:52:41 +0300

    [fix] Добавлены комментарии в 9 задание из семистровок

--- FALL2017/SEM (seminars)/part1_task9/part1_task9/Program.cs
+++ FALL2017/SEM (seminars)/part1_task9/part1_task9/Program.cs
@@ -11,22 +11,22 @@ namespace part1_task9
         static void Main(string[] args)
         {
             Console.WriteLine("a * x^2 + b * x + c");
-            Console.Write("a=");
+            Console.Write("a=");  // Ввод коэффициентов
             int a = Convert.ToInt32(Console.ReadLine());
             Console.Write("b=");
             int b = Convert.ToInt32(Console.ReadLine());
             Console.Write("c=");
             int c = Convert.ToInt32(Console.ReadLine());
-            String Formula = null;
-            if (a != 0)
+            String Formula = null; // Для случаев, когда все коэффициенты равны нулю
+            if (a != 0) 
             {
-                Formula = a + " * x^2 ";
+                Formula = a + " * x^2 "; // Проверка первого элемент
             }
             if (b != 0)
             {
                 if (b < 0 | a == 0)
                 {
-                    Formula = Formula + b + " * x ";
+                    Formula = Formula + b + " * x "; // Проверка второго элемент
                 }
                 else
                     Formula = Formula + " + " + b + " * x ";
@@ -35,12 +35,12 @@ namespace part1_task9
             {
              if (c < 0 | b == 0 & a == 0)
                 {
-                Formula = Formula + " " + c;
+                Formula = Formula + " " + c; // Проверка третьего элемента
                 }
                 else
                     Formula = Formula + " + " + c;
             }
-            if (Formula == null)
+            if (Formula == null) // Проверка уравнения на равность нулю
             {
                 Console.Write("Любое число является решением уравнения.");
             }
