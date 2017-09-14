2b323e689da1b4b938a7b073f4cb8bd9ec75b4bf
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-14 12:09:45 +0300

    [add][del] Решено 7 задание и удалены тестовые файлы

---
+++ FALL2017/PR (practice)/First tasks/Task_7/Task_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x1=");
+            double x1 = Convert.ToDouble(Console.Read());
+            Console.WriteLine("y1=");
+            double y1 = Convert.ToDouble(Console.Read());
+            Console.WriteLine("x2=");
+            double x2 = Convert.ToDouble(Console.Read());
+            Console.WriteLine("y2=");
+            double y2 = Convert.ToDouble(Console.Read());
+            Console.WriteLine((x2 - x1) + ';' + (y2 - y1));
+            Console.WriteLine((y2 - y1) + ';' + (-1) * (x2 - x1));
+            Console.Read();
+        }
+    }
+}
