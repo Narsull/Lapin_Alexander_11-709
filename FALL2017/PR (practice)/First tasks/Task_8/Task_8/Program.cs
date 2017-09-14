b0cc8c77ad084c6971dba0611680e3b72426700a
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-14 12:43:35 +0300

    [add] Решено 9 задание из семистровок

--- FALL2017/PR (practice)/First tasks/Task_8/Task_8/Program.cs
+++ FALL2017/PR (practice)/First tasks/Task_8/Task_8/Program.cs
@@ -10,6 +10,14 @@ namespace Task_8
     {
         static void Main(string[] args)
         {
+            Double x1 = double.Parse(Console.ReadLine());
+            Double y1 = double.Parse(Console.ReadLine());
+            Double x2 = double.Parse(Console.ReadLine());
+            Double y2 = double.Parse(Console.ReadLine());
+            Double x3 = double.Parse(Console.ReadLine());
+            Double y3 = double.Parse(Console.ReadLine());
+            double a = (y2 - y1)(x2 - x1);
+            do
         }
     }
 }
