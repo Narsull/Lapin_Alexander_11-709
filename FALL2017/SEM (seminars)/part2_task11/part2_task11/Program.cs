3ea73448cce478b42eabaf6c8ffcfea200d14db6
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-16 16:04:21 +0300

    [add] Решено 11 задание 2 части из семистровок.

---
+++ FALL2017/SEM (seminars)/part2_task11/part2_task11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2_task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двоичное число.");
+            int paskal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите десятичное натуральное число.");
+            int k = Convert.ToInt32(Console.ReadLine());
+            string str = Convert.ToString(paskal, 2);
+            int laksap = Convert.ToInt32(str, 2);
+            if (laksap % k == 0)
+            {
+                Console.WriteLine("Ввведённое двоичное число делится на число k нацело.");
+            }
+            else Console.WriteLine("Ввведённое двоичное число не делится на число k нацело.");
+            Console.Read();
+        }
+    }
+}
