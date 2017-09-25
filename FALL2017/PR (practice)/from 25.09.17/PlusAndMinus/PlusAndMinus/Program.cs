6f9e5720d34fba43e3aee05b0509b49e719b1d4a
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-25 09:15:37 +0300

    [add] The problem with negative and positive elements is solved.

---
+++ FALL2017/PR (practice)/from 25.09.17/PlusAndMinus/PlusAndMinus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusAndMinus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int plus = 0;
+            int minus = 0;
+            for (int a = 0; a != n; a++)
+            {
+                array[a] = Convert.ToInt32(Console.ReadLine());
+                if (array[a] > 0)
+                    plus = plus + 1;
+                if (array[a] < 0)
+                    minus = minus + 1;
+            }
+            if (plus > minus)
+                Console.WriteLine("Положительных элементов больше отрицательных.");
+            if (plus < minus)
+                Console.WriteLine("Отрицательных элементов больше положительных.");
+            if (plus == minus)
+                Console.WriteLine("Количество положительных элементов равно количеству отрицательных.");
+            Console.Read();
+        }
+    }
+}
