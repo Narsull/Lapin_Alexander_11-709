e76dab048e2fecc2b04250d6daac658dac3187a0
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-25 08:57:07 +0300

    [add] Solving the problem with an array

---
+++ FALL2017/PR (practice)/from 25.09.17/Massive/Massive/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            int amount = 0;
+            for (int a = 0; a != n; a++)
+            {
+                array[a] = Convert.ToInt32(Console.ReadLine());
+                amount = amount + array[a];
+            }
+            Console.WriteLine(amount);
+            Console.Read();
+        }
+    }
+}
