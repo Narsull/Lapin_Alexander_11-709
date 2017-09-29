4618376b7f9007f0d665c6ad346910f4d70c7706
Lapin Alexander <alexlapin1999@gmail.com>
2017-09-29 18:49:26 +0300

    [add] added projects from ulearn.me

---
+++ FALL2017/PR (practice)/ULearn Projects/Billard/BilliardsTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Billiards
+{
+	public static class BilliardsTask
+	{
+		public static double BounceWall(double directionRadians, double wallInclinationRadians)
+		{
+            //That's work.
+            // I think.
+            // I bealive.
+            return 2 * directionRadians - wallInclinationRadians;
+		}
+	}
+}
---
+++ FALL2017/PR (practice)/ULearn Projects/Distance/DistanceTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DistanceTask
+{
+    public static class DistanceTask
+    {
+        // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+        public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+        {
+            double distance = 1;
+            double aPoint = Math.Sqrt(((x - ax) * (x - ax)) + ((y - ay) * (y - ay)));
+            double bPoint = Math.Sqrt(((x - bx) * (x - bx)) + ((y - by) * (y - by)));
+            double ab = Math.Sqrt(((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)));
+            if (ax == bx)
+            {
+                if (((y >= by) && (y <= by)) || ((y <= ay) && (y >= by)))
+                {
+                    distance = 0;
+                }
+            }
+            else
+            {
+                double a = (ay - by) / (ax - bx);
+                double b = ((ay + by) - a * (ax + bx)) / 2;
+                if (((y == a * x + b) && (x > ax) && (x < bx)) || ((y == a * x + b) && (bx > ax) && (x < ax)))
+                    distance = 0;
+            }
+            if (distance != 0)
+            {
+                if (x <= bx && x >= ax && ab != 0)
+                {
+                    double perimeter = (aPoint + bPoint + ab) / 2;
+                    double square = Math.Sqrt((perimeter * (perimeter - aPoint) * (perimeter - bPoint) * (perimeter - ab)));
+                    distance = (2 * square) / ab;
+                }
+                else
+                {
+                    distance = Math.Min(aPoint, bPoint);
+                }
+            }
+            return distance;
+        }
+    }
+}
