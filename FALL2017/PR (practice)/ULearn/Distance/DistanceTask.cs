598d7b2326d1120ff469ef12dc6fefc484bd5cc7
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-01 20:41:23 +0300

    [fix] continied solving of task 10 part 3

--- FALL2017/PR (practice)/ULearn/Distance/DistanceTask.cs
+++ FALL2017/PR (practice)/ULearn/Distance/DistanceTask.cs
@@ -10,19 +10,23 @@ namespace DistanceTask
             double aPoint = Math.Sqrt(((x - ax) * (x - ax)) + ((y - ay) * (y - ay)));
             double bPoint = Math.Sqrt(((x - bx) * (x - bx)) + ((y - by) * (y - by)));
             double ab = Math.Sqrt(((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)));
-            if (((x - ax) * (by - ay) - (y - ay) * (bx - ax) == 0) && ((ax < x && x < bx) || (bx < x && x < ax)))
+            if (((x - ax) * (by - ay) - (y - ay) * (bx - ax) == 0) && (ax < x && x < bx) || (bx < x && x < ax))
             {
                 return 0;
             }
-            else if (x <= bx && x >= ax && ab != 0)
+            else
             {
                 double perimeter = (aPoint + bPoint + ab) / 2;
                 double square = Math.Sqrt((perimeter * (perimeter - aPoint) * (perimeter - bPoint) * (perimeter - ab)));
-                return (2 * square) / ab;
-            }
-            else
-            {
-                return Math.Min(aPoint, bPoint);
+                double distance = (2 * square) / ab;
+                if ((2 * square / ab) > Math.Min(aPoint, bPoint))
+                {
+                    return Math.Min(aPoint, bPoint);
+                }
+                else
+                {
+                    return (2 * square) / ab;
+                }
             }
         }
     }
--- FALL2017/SEM (seminars)/Plan/part3_task10/part3_task10/Program.cs
+++ FALL2017/SEM (seminars)/Plan/part3_task10/part3_task10/Program.cs
@@ -10,12 +10,20 @@ namespace part3_task10
     {
         static void Main(string[] args)
         {
-
-            for (int a = 0; str = "Stop"; a++)
+            int number = 0;
+            int number1 = 0;
+            int i = 0;
+            Console.WriteLine("Напишите последовательнось положительныч и отрицетльных чисел.");
+            Console.WriteLine("Напишите ноль, если ваша последовательность закончена.");
+            for (int a = 0; number == 0; a++)
             {
-
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number > 0 && number1 < 0 || number < 0 && number1 > 0)
+                    i = i + 1;
+                number1 = number;
             }
-
+            Console.WriteLine(i);
+            Console.Read();
         }
     }
 }
