928ef00435d825ab02dd7219cc4195048ee6e4dc
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-01 23:41:08 +0300

    [fix] solved task 10 part 3

--- FALL2017/SEM (seminars)/Plan/part3_task10/part3_task10/Program.cs
+++ FALL2017/SEM (seminars)/Plan/part3_task10/part3_task10/Program.cs
@@ -10,19 +10,19 @@ namespace part3_task10
     {
         static void Main(string[] args)
         {
-            int number = 0;
-            int number1 = 0;
+            int number = 1;
+            int number1 = 1;
             int i = 0;
-            Console.WriteLine("Напишите последовательнось положительныч и отрицетльных чисел.");
+            Console.WriteLine("Напишите последовательнось положительных и отрицетльных чисел.");
             Console.WriteLine("Напишите ноль, если ваша последовательность закончена.");
-            for (int a = 0; number == 0; a++)
+            while (number != 0)
             {
                 number = Convert.ToInt32(Console.ReadLine());
                 if (number > 0 && number1 < 0 || number < 0 && number1 > 0)
                     i = i + 1;
                 number1 = number;
             }
-            Console.WriteLine(i);
+            Console.WriteLine("Произошло " + i + " изменений(ия) знака");
             Console.Read();
         }
     }
