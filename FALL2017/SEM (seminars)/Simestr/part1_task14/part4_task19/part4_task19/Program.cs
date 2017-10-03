535d1d55cb3ec1f88fc404afed77d0abe65ab077
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-03 20:21:48 +0300

    [add][fix] added task 9 part 4, solved task 19 part 4 and fixed directory.

---
+++ FALL2017/SEM (seminars)/Simestr/part1_task14/part4_task19/part4_task19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part4_task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int divider = 1;
+            int a = 1;
+            while (a < number)
+            {
+                if (number % a == 0)
+                {
+                    divider = a;
+                }
+                a++;
+            }
+            Console.WriteLine(divider);
+            Console.Read();
+        }
+    }
+}
---
+++ FALL2017/SEM (seminars)/Simestr/part1_task14/part4_task9/part4_task9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part4_task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
