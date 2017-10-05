d5d2bd723b5bcf504b83a42916738a98789194c0
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-05 16:50:56 +0300

    [fix] fixed directory

---
+++ FALL2017/PR (practice)/My Little Projects/Hello World/Hello World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Say your name, please.");
+            Console.WriteLine();
+            name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Hello , " + name + "!");
+            Console.Read();
+        }
+    }
+}
