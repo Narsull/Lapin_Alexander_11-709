a34ddfd69387c9670652619515f9c3f2c4c6344f
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-06 13:36:02 +0300

    [fix] continied solving of task Distance

--- FALL2017/MISC (miscellaneous)/ULearn/Distance/DistanceTask.cs
+++ FALL2017/MISC (miscellaneous)/ULearn/Distance/DistanceTask.cs
@@ -7,17 +7,17 @@ namespace DistanceTask
         // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
         public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
         {
-            double aPoint = Math.Sqrt(((x - ax) * (x - ax)) + ((y - ay) * (y - ay)));
-            double bPoint = Math.Sqrt(((x - bx) * (x - bx)) + ((y - by) * (y - by)));
-            double ab = Math.Sqrt(((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)));
-            double a = Math.Acos((Math.Pow(bPoint, 2) - Math.Pow(aPoint, 2) + Math.Pow(ab, 2)) / (2 * aPoint * bPoint));
-            double b = Math.Acos((Math.Pow(aPoint, 2) - Math.Pow(bPoint, 2) + Math.Pow(ab, 2)) / (2 * aPoint * ab));
-            double point = Math.Acos((Math.Pow(aPoint, 2) - Math.Pow(bPoint, 2) + Math.Pow(ab, 2)) / (2 * aPoint * ab));
-            if (((x - ax) * (by - ay) - (y - ay) * (bx - ax) == 0) && (ax < x && x < bx) || (bx < x && x < ax))
+            double aPoint = Math.Sqrt(Math.Pow(x - ax, 2) + Math.Pow(y - ay, 2));
+            double bPoint = Math.Sqrt(Math.Pow(x - bx, 2) + Math.Pow(y - by, 2));
+            double ab = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2));
+
+            double cosPoint = (Math.Pow(aPoint, 2) + Math.Pow(bPoint, 2) - Math.Pow(ab, 2)) / (2 * aPoint * bPoint);
+
+            if ((ax - x) / (bx - x) == (ay - y) / (by - y) && (aPoint + bPoint == ab))
             {
                 return 0;
             }
-            else if ( && ab != 0)
+            else if (cosPoint > 0)
             {
                 double perimeter = (aPoint + bPoint + ab) / 2;
                 double square = Math.Sqrt((perimeter * (perimeter - aPoint) * (perimeter - bPoint) * (perimeter - ab)));
