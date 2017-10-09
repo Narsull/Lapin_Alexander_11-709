75f42668750f288da250ce385ffed013a3acdb2c
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-09 17:26:56 +0300

    [add] solved task for next seminar

--- FALL2017/MISC (miscellaneous)/ULearn/Maze/DiagonalMazeTask.cs
+++ FALL2017/MISC (miscellaneous)/ULearn/Maze/DiagonalMazeTask.cs
@@ -1,9 +1,46 @@
 ﻿namespace Mazes
 {
-	public static class DiagonalMazeTask
-	{
-		public static void MoveOut(Robot robot, int width, int height)
-		{
-		}
-	}
+    public static class DiagonalMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+
+        }
+
+        public static void MoveRight(Robot robot, int width, int height)
+        {
+            int i = 1;
+            if (width > height)
+            {
+                i = (height + width - 4) / height;
+            }
+            else
+            {
+                i = (height + width - 4) / width;
+            }
+            for (int a = 1; a <= i; i++)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+        }
+
+        public static void MoveDown(Robot robot, int width, int height)
+        {
+            {
+                int i = 1;
+                if (width > height)
+                {
+                    i = (height + width - 4) / height;
+                }
+                else
+                {
+                    i = (height + width - 4) / width;
+                }
+                for (int a = 1; a <= i; i++)
+                {
+                    robot.MoveTo(Direction.Right);
+                }
+            }
+        }
+    }
 }
---
+++ FALL2017/SEM (seminars)/from 09.10.17/from 09.10.17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace from_09._10._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            if (n >= 1 && n <= 27)
+            {
+                for (int a = 100; a < 1000; a++)
+                {
+                    string aString = Convert.ToString(a);
+                    int number0 = Convert.ToInt32(aString.Substring(0, 1));
+                    int number1 = Convert.ToInt32(aString.Substring(1, 1));
+                    int number2 = Convert.ToInt32(aString.Substring(2, 1));
+
+                    int amount = number0 + number1 + number2;
+                    if (amount == n)
+                        count++;
+                }
+                Console.WriteLine(count);
+            }
+            else Console.WriteLine("Error");
+            Console.Read();
+        }
+    }
+}
