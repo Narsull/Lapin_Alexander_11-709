a9eb8e0289769eda46e2797d6dd0b93e858978b9
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-12 18:39:28 +0300

    [fix] fixed solving of task DiagonalMazeTask

--- FALL2017/MISC (miscellaneous)/ULearn/Maze/DiagonalMazeTask.cs
+++ FALL2017/MISC (miscellaneous)/ULearn/Maze/DiagonalMazeTask.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Прошу прощения за костыли.
+// В данный момент не мыслей о том, как от них избавиться.
+
+using System;
 
 namespace Mazes
 {
@@ -8,47 +11,51 @@ namespace Mazes
         {
             height -= 2;
             width -= 2;
-
-            if (height > width)
-                MoveDown(robot, width, height);
-            else
-                MoveRight(robot, width, height);
-        }
-
-        public static void MoveRight(Robot robot, int width, int height)
-        {
             int i;
 
             if (height / width == 1 && width / height == 0 || height / width == 0 && width / height == 1)
+            {
                 i = 1;
+
+                for (int c = 1; c <= width; c++)
+                {
+                    MoveDown(robot, width, height, i, c);
+                }
+            }
+            else if (height > width)
+            {
+                i = (height + width + 4) / (width + 2);
+
+                for (int c = 1; c <= width; c++)
+                {
+                    MoveDown(robot, width, height, i, c);
+                }
+            }
             else
+            {
                 i = (height + width + 4) / (height + 2);
 
-            for (int c = 1; c <= height; c++)
-            {
+                for (int c = 1; c <= height; c++)
+                {
+                    MoveRight(robot, width, height, i, c);
+                }
+            }
+        }
+
+        public static void MoveRight(Robot robot, int width, int height, int i, int c)
+        {
                 for (int a = 0; a < i; a++)
                     robot.MoveTo(Direction.Right);
                 if (c < height)
                     robot.MoveTo(Direction.Down);
-            }
         }
 
-        public static void MoveDown(Robot robot, int width, int height)
+        public static void MoveDown(Robot robot, int width, int height, int i, int c)
         {
-            int i;
-
-            if (height / width == 1 && width / height == 0 || height / width == 0 && width / height == 1)
-                i = 1;
-            else
-                i = (height + width + 4) / (width + 2);
-
-            for (int c = 1; c <= width; c++)
-            {
                 for (int a = 0; a < i; a++)
                     robot.MoveTo(Direction.Down);
                 if (c < width)
                     robot.MoveTo(Direction.Right);
-            }
         }
     }
 }
