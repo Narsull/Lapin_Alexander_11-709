26df702b9022a99f7ea9bf8194a350e9a536d385
Lapin Alexander <alexlapin1999@gmail.com>
2017-10-18 00:29:02 +0300

    [add] added tasks from seminars

---
+++ FALL2017/SEM (seminars)/from 16.10.17/from 16.10.2017/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace from_16._10._2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Maximum(number));
+            Console.WriteLine();
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Numbers(a, b, c);
+            Console.WriteLine();
+
+            Table();
+
+            Console.Read();
+        }
+
+        public static int Maximum(int number)
+        {
+            int max = 0;
+            string numberString = Convert.ToString(number);
+            char[] numberChar = new char[numberString.Length];
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                int a = Convert.ToInt32(numberChar[i]);
+                if (a > max)
+                    max = a;
+            }
+            return max;
+        }
+
+        public static void Numbers(int a, int b, int c)
+        {
+            int number = c - a;
+            while (number < b)
+            {
+                number = number + a;
+                Console.WriteLine(number);
+            }
+        }
+
+        public static void Table()
+        {
+            for (int a = 1; a < 10; a++)
+            {
+                for (int b = 1; b < 10; b++)
+                {
+                    Console.Write(a * b + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ FALL2017/SEM (seminars)/from 17.10.17/from 17.10.17/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace from_17._10._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            MaxAmount();
+        }
+
+        static void AAndB()
+        {
+            bool number = false;
+            string a = Convert.ToString(Console.ReadLine());
+            string b = Convert.ToString(Console.ReadLine());
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                    if (b.Remove(i, 1) == a)
+                    {
+                        number = true;
+                        break;
+                    }
+            }
+
+            Console.WriteLine(number);
+            Console.Read();
+        }
+
+        static void MaxAmount()
+        {;
+            int maxAmount = 0;
+            int[] array = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                int amount = 0;
+                array[i] = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    amount = amount + array[j];
+                    if (amount > maxAmount)
+                        maxAmount = amount;
+                    amount = 0;
+                }
+            }
+            Console.WriteLine(maxAmount);
+            Console.Read();
+        }
+    }
+}
